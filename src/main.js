8cdfd7e3f8e35b451c11232a50b5bfc8a7346309
ledgerbot2020 <zerjioang@users.noreply.github.com>
2020-05-28 20:59:52 +0200

    added more routers to prerender block
    Signed-off-by: ledgerbot2020 <zerjioang@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -76,6 +76,5 @@ const app = new Vue({
     i18n,
     render: h => h(App),
   mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
-  mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
 }).$mount('#app')
 // Now the app has started!
