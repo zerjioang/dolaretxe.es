42d547b4ba271e8582e37906c6ac2ac4639148e2
zerjioang <zerjioang@users.noreply.github.com>
2020-07-18 16:14:01 +0200

    footer traducido
    Signed-off-by: zerjioang <zerjioang@users.noreply.github.com>

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,15 +1,16 @@
 <template>
   <footer class="ftco-footer ftco-bg-dark ftco-section black-gradient">
-    <cookie-law theme="dark-lime" buttonText="Aceptar">
+    <cookie-law theme="dark-lime" :buttonText="$t('aceptar')">
       <div slot="message">
         <p class="justified cookie white">
-          Utilizamos cookies propias y de terceros para mejorar nuestros servicios. Si continua navegando, consideramos que acepta su uso. Puede cambiar la configuración mediante los ajustes de su navegador u obtener
+          {{$t('cookie_text')}}
           <router-link :to="{ name: 'cookies' }" alt="mas información sobre cookies">
-            <span style="color: white;">más información</span>
+            <span style="color: white;">{{$t('cookie_more_info')}}</span>
           </router-link>.
         </p>
       </div>
     </cookie-law>
+
     <div class="container">
       <div class="row mb-5">
         <div class="col-md-12">
@@ -26,10 +27,10 @@
       <div class="row mb-5">
         <div class="col-md">
           <div class="ftco-footer-widget mb-4">
-            <h2 class="ftco-heading-2">Dolaretxe Haur Eskola</h2>
+            <h2 class="ftco-heading-2">{{$t('title')}}</h2>
             <p
               align="justify"
-            >Os invitamos a conocer la Escuela Infantil Dolaretxe Haur Eskola, donde os proporcionaremos una asistencia más personalizada y os informaremos de todos servicios.</p>
+            >{{$t('description')}}</p>
             <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
               <li v-show="false" class="ftco-animate">
                 <a href="#" rel="noopener" target="_blank">
@@ -55,42 +56,42 @@
         </div>
         <div class="col-md">
           <div class="ftco-footer-widget mb-4 ml-md-5">
-            <h2 class="ftco-heading-2">Información</h2>
+            <h2 class="ftco-heading-2">{{$t('info_title')}}</h2>
             <ul class="list-unstyled">
               <li>
-                <router-link :to="{name:'jornadas'}" class="py-2 d-block">Jornadas</router-link>
+                <router-link :to="{name:'jornadas'}" class="py-2 d-block">{{$t('menuitem_jornadas')}}</router-link>
               </li>
               <li>
-                <router-link :to="{name:'escuela'}" class="py-2 d-block">Escuela</router-link>
+                <router-link :to="{name:'escuela'}" class="py-2 d-block">{{$t('menuitem_escuela')}}</router-link>
               </li>
               <li>
-                <router-link :to="{name:'unicef'}" class="py-2 d-block">UNICEF</router-link>
+                <router-link :to="{name:'unicef'}" class="py-2 d-block">{{$t('menuitem_uNICEF')}}</router-link>
               </li>
               <li>
-                <router-link :to="{name:'galeria'}" class="py-2 d-block">Fotos</router-link>
+                <router-link :to="{name:'galeria'}" class="py-2 d-block">{{$t('menuitem_fotos')}}</router-link>
               </li>
             </ul>
           </div>
         </div>
         <div class="col-md">
           <div class="ftco-footer-widget mb-4">
-            <h2 class="ftco-heading-2">Información Legal</h2>
+            <h2 class="ftco-heading-2">{{$t('legal_title')}}</h2>
             <ul class="list-unstyled">
               <li>
-                <router-link :to="{'name':'legal'}" class="py-2 d-block">Aviso Legal</router-link>
+                <router-link :to="{'name':'legal'}" class="py-2 d-block">{{$t('menuitem_aviso')}}</router-link>
               </li>
               <li>
-                <router-link :to="{'name':'privacy'}" class="py-2 d-block">Pólitica de privacidad</router-link>
+                <router-link :to="{'name':'privacy'}" class="py-2 d-block">{{$t('menuitem_privacidad')}}</router-link>
               </li>
               <li>
-                <router-link :to="{'name':'cookies'}" class="py-2 d-block">Información sobre Cookies</router-link>
+                <router-link :to="{'name':'cookies'}" class="py-2 d-block">{{$t('menuitem_cookies')}}</router-link>
               </li>
             </ul>
           </div>
         </div>
         <div class="col-md">
           <div class="ftco-footer-widget mb-4">
-            <h2 class="ftco-heading-2">Contáctanos!</h2>
+            <h2 class="ftco-heading-2">{{$t('contact_title')}}</h2>
             <div class="block-23 mb-3">
               <ul>
                 <li>
@@ -108,9 +109,7 @@
                         target="_blank"
                         rel="noopener"
                       >
-                        Escuela Infantil Haur Eskola.
-                        Calle Prim, 11. Bajo.
-                        48006 Bilbao. Bizkaia
+                        {{$t('direccion')}}
                       </a>
                     </span>
                   </a>
@@ -153,7 +152,7 @@
       </div>
       <div class="row">
         <div class="col-md-12 text-center copyright">
-          <p class="bold">Copyright &copy; {{currentYear}}. Todos los derechos reservados.</p>
+          <p class="bold">Copyright &copy; {{currentYear}}. {{$t('copyright_text')}}</p>
         </div>
       </div>
     </div>
@@ -238,14 +237,62 @@ footer {
 
 <i18n>
 {
-  "es":{
-
-  },
-  "en":{
-    
-  },
-  "eus":{
-    
-  }
+	"es": {
+		"cookie_text": "Utilizamos cookies propias y de terceros para mejorar nuestros servicios. Si continua navegando, consideramos que acepta su uso. Puede cambiar la configuración mediante los ajustes de su navegador u obtener",
+		"cookie_more_info": "más información",
+		"aceptar": "Aceptar",
+		"title": "Escuela Infantil Dolaretxe",
+		"description": "Os invitamos a conocer la Escuela Infantil Dolaretxe, donde os proporcionaremos una asistencia más personalizada y os informaremos de todos servicios.",
+		"info_title": "Información",
+		"legal_title": "Información Legal",
+		"contact_title": "Contáctanos!",
+		"menuitem_jornadas": "Jornadas",
+		"menuitem_escuela": "Escuela",
+		"menuitem_uNICEF": "UNICEF",
+		"menuitem_fotos": "Fotos",
+		"menuitem_aviso": "Aviso Legal",
+		"menuitem_privacidad": "Pólitica de privacidad",
+		"menuitem_cookies": "Información sobre Cookies",
+		"direccion": "Escuela Infantil. Calle Prim, 11. Bajo. 48006 Bilbao. Bizkaia",
+		"copyright_rext": "Todos los derechos reservados."
+	},
+	"en": {
+		"cookie_text": "",
+		"cookie_more_info": "",
+		"aceptar": "",
+		"title": "",
+		"description": "",
+		"info_title": "",
+		"legal_title": "",
+		"contact_title": "",
+		"menuitem_jornadas": "",
+		"menuitem_escuela": "",
+		"menuitem_uNICEF": "",
+		"menuitem_fotos": "",
+		"menuitem_aviso": "",
+		"menuitem_privacidad": "",
+		"menuitem_cookies": "",
+		"direccion": "",
+		"copyright_rext": ""
+	},
+	"eus": {
+		"cookie_text": "Norberaren eta hirugarrenen cookie-ak erabiltzen ditugu gure zerbitzuak hobetzeko. Nabigatzen jarraitzen baduzu, zure erabilera onartzen duzula uste dugu. Konfigurazioa alda dezakezu zure nabigatzailearen konfigurazioaren bidez",
+		"cookie_more_info": "informazio gehiago",
+		"aceptar": "Onartu",
+		"title": "Dolaretxe Haur Eskola",
+		"description": "Dolaretxe Haur Eskola ezagutzera gonbidatzen zaituztegu. Bertan, laguntza pertsonalizatuagoa emango dizuegu eta zerbitzu guztien berri emango dizuegu.",
+		"info_title": "Informazioa",
+		"legal_title": "Lehe Oharrak",
+		"contact_title": "Kontaktatu gaitzazu!",
+		"menuitem_jornadas": "Jardunaldiak",
+		"menuitem_escuela": "Eskola",
+		"menuitem_uNICEF": "UNICEF",
+		"menuitem_fotos": "Argazkiak",
+		"menuitem_aviso": "Lege Oharra",
+		"menuitem_privacidad": "Pribatutasun Politika",
+		"menuitem_cookies": "Cookie-ei buruzko informazioa",
+		"direccion": "Escuela Infantil Haur Eskola. Prim Kalea, 11. Bajo. 48006 Bilbao. Bizkaia",
+		"copyright_rext": "Eskubide guztiak erreserbatuta."
+	}
 }
 </i18n>
